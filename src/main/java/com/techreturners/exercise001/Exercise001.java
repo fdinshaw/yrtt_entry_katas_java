022ac24bd267a44b638c314ff13092998b8f4c23
MacHD <MacHD@ferhizs-mbp>
2020-10-29 20:35:32 +0000

    Exercise001 complete

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -1,5 +1,8 @@
 package com.techreturners.exercise001;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Exercise001 {
 
     // In this Kata, you will be given an array of numbers in which two numbers occur once and the rest occur only twice. 
@@ -10,8 +13,26 @@ public class Exercise001 {
 
     // Good luck!
 
-    public int singles(int [] arr){
-        // Add your code here!
-        return 0;
-    }
+	public int singles(int [] arr){
+		// Add your code here!
+		System.out.println("test");
+		HashMap<Integer, Integer> occurs =new HashMap<Integer, Integer>();
+
+		for(int i=0;i<arr.length;i++) {
+			Integer count=occurs.get(arr[i]);
+			if(count==null) 
+				occurs.put(arr[i], 1);
+			else if(count>0) 
+				occurs.put(arr[i], count+1);
+		}
+
+		int sum=0;
+		for(Integer key : occurs.keySet()) {
+			if(occurs.get(key)==1)
+				sum=sum+key;
+		}
+
+		return sum;
+
+	}
 }
